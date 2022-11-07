f78777d2b2f13b1305fed04b80d09e8557002784
hassanng-dev <hassanngdev@gmail.com>
2022-11-06 23:46:14 -0500

    first commit for savage auth project

---
+++ config/database.js
@@ -0,0 +1,8 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://HassanNgDev:Has0424686!@cluster0.fpl4oh8.mongodb.net/savage?retryWrites=true&w=majority', 
+    'dbName': 'savage'
+
+    
+};
